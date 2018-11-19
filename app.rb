783258648a1ab49e135abee2f1d12a81a8ce25af
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-11-19 15:12:10 +0000

    Added bookmarks erb page

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
     "Hello World!"
   end
+
+  get '/bookmarks' do
+    erb(:bookmarks)
+  end
+
   run! if app_file == $0
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,4 +3,8 @@ feature 'Testing index page' do
     visit('/')
     expect(page).to have_content("Hello World!")
   end
+  scenario "Viewing our bookmarks" do
+    visit('/bookmarks')
+    expect(page).to have_content("http://www.google.com/")
+  end
 end
