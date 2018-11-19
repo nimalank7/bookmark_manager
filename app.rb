64ef1ecdc5ff941ca9494ee8c62bac7dfde32f88
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-11-19 16:30:55 +0000

    now displays list of bookmarks from model

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/bookmarks' do
+    @new_list = List.create_instance
+    bookmark_1 = Bookmark.new("http://www.google.com/")
+    bookmark_2 = Bookmark.new("http://www.microsoft.com/")
+    @new_list.contents << bookmark_1
+    @new_list.contents << bookmark_2
+    @new_list = List.read_instance.see_list
     erb(:bookmarks)
   end
 
