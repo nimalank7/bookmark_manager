c3e4a8808d4e5930e1b0a4dc401e8fdac932f937
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-12-01 15:06:30 +0000

    databoase model properly finished

--- lib/bookmark_tag_model.rb
+++ lib/bookmark_tag_model.rb
@@ -1,4 +1,13 @@
 require_relative "../lib/database_connection.rb"
 
 class BookmarkTag
+  attr_reader :bookmark_id, :tag_id
+  def initialize(bookmark_id, tag_id)
+    @bookmark_id = bookmark_id
+    @tag_id = tag_id
+  end
+  def self.create(bookmark_id, tag_id)
+    result = DatabaseConnection.query(("INSERT INTO Bookmark_Tag (bookmark_id, tag_id) VALUES('#{bookmark_id}', '#{tag_id}') RETURNING bookmark_id, tag_id;"))
+    BookmarkTag.new(result[0]['bookmark_id'], result[0]['tag_id'])
+  end
 end
--- spec/bookmark_tag_model_spec.rb
+++ spec/bookmark_tag_model_spec.rb
@@ -1,6 +1,19 @@
 require "bookmark_tag_model"
 describe BookmarkTag do
-  it "creates an instance of the BookmarkTag class" do
-    expect(BookmarkTag.new.instance_of? BookmarkTag).to eq true
+  before(:each){add_bookmarks_and_comments_and_users}
+  it "user sets instance variable of bookmark_id to 1" do
+    bookmark_tag = BookmarkTag.new(1, 1)
+    expect(bookmark_tag.bookmark_id).to eq 1
+  end
+  it "user sets instance variable of tag_id to 1" do
+    bookmark_tag = BookmarkTag.new(1, 1)
+    expect(bookmark_tag.tag_id).to eq 1
+  end
+  it "creates a bookmark tag and returns bookmark_id of 1" do
+    expect(BookmarkTag.create(1,1).bookmark_id).to eq "1"
+  end
+  it "creates a bookmark tag and returns a tag_id of 1" do
+    bookmark_tag = BookmarkTag.new(1, 1)
+    expect(BookmarkTag.create(1,1).tag_id).to eq "1"
   end
 end
